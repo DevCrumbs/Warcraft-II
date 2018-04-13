1e36cbfe5ecab7bc7e264d1f6cba471b79246b00
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-04-13 20:45:37 +0200

    iPoint operators

--- Warcraft II/Warcraft II/p2Point.h
+++ Warcraft II/Warcraft II/p2Point.h
@@ -41,7 +41,7 @@ public:
 	// Math ------------------------------------------------
 	p2Point operator -(const p2Point &v) const
 	{
-		p2Vector2 r;
+		p2Point r;
 
 		r.x = x - v.x;
 		r.y = y - v.y;
@@ -51,7 +51,7 @@ public:
 
 	p2Point operator + (const p2Point &v) const
 	{
-		p2Vector2 r;
+		p2Point r;
 
 		r.x = x + v.x;
 		r.y = y + v.y;
@@ -83,6 +83,16 @@ public:
 	bool operator !=(const p2Point& v) const
 	{
 		return (x != v.x || y != v.y);
+	}
+
+	bool operator ==(const int& v) const
+	{
+		return (x == v && y == v);
+	}
+
+	bool operator !=(const int& v) const
+	{
+		return (x != v || y != v);
 	}
 
 	// Utils ------------------------------------------------
