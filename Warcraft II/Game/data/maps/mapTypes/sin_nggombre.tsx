f7e2e89ecb0254f46099b6597f7e3a667470aef9
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-04-15 23:21:21 +0200

    Revert "delete resources"
    This reverts commit d42bcc5dc1c81b41beba03c5d228ba6a3b43d4b9.

---
+++ Warcraft II/Game/data/maps/mapTypes/sin_nggombre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="wastelandTiles" tilewidth="32" tileheight="32" spacing="1" tilecount="380" columns="19">
+ <image source="../../../../../../Warcraft-II-de83b58db1fca15866c1ee864fe9eb1a05073a8e/Warcraft II/Game/data/wastelandTiles.png" width="626" height="659"/>
+</tileset>
