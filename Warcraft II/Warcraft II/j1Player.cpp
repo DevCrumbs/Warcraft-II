2bbe5b936eeadef778599bc405c15f5579cb53c9
JoanValiente <joan.valiente.lorite@gmail.com>
2018-04-15 16:51:36 +0200

    if

--- Warcraft II/Warcraft II/j1Player.cpp
+++ Warcraft II/Warcraft II/j1Player.cpp
@@ -800,10 +800,11 @@ void j1Player::OnDynamicEntitiesEvent(DynamicEntity* dynamicEntity, EntitiesEven
 }
 void j1Player::RescuePrisoner(TerenasDialogEvents dialogEvent, SDL_Rect iconText, iPoint iconPos) {
 
-	if(App->scene->terenasDialogEvent != dialogEvent)
-	App->scene->UnLoadTerenasDialog();
-	App->scene->terenasDialogTimer.Start();
-	App->scene->LoadTerenasDialog(dialogEvent);
+	if (App->scene->terenasDialogEvent != dialogEvent) {
+		App->scene->UnLoadTerenasDialog();
+		App->scene->terenasDialogTimer.Start();
+		App->scene->LoadTerenasDialog(dialogEvent);
+	}
 
 	UIImage_Info imageInfo;
 	imageInfo.texArea = iconText;
