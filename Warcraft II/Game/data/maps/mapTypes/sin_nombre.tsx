d42bcc5dc1c81b41beba03c5d228ba6a3b43d4b9
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-04-15 22:27:24 +0200

    delete resources

--- Warcraft II/Game/data/maps/mapTypes/sin_nombre.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="logic" tilewidth="32" tileheight="32" spacing="1" tilecount="16" columns="4">
- <image source="../rooms/logic.png" width="160" height="160"/>
-</tileset>
