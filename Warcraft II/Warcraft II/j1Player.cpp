2ad7d007d66a0edeff66c499bda328a77415c74a
ValdiviaDev <d.valdiviamartinez@gmail.com>
2018-04-15 14:16:19 +0200

    some sounds changed

--- Warcraft II/Warcraft II/j1Player.cpp
+++ Warcraft II/Warcraft II/j1Player.cpp
@@ -455,7 +455,7 @@ void j1Player::CheckUnitSpawning()
 					fPoint pos = { (float)barracksTilePos.x,(float)barracksTilePos.y };
 
 					App->entities->AddEntity(EntityType_ELVEN_ARCHER, pos, (EntityInfo&)App->entities->GetUnitInfo(EntityType_ELVEN_ARCHER), unitInfo, this);
-					App->audio->PlayFx(21, 0);
+					App->audio->PlayFx(18, 0);
 				}
 			}
 				break;
