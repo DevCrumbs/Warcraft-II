76e140009da93107f00b5f496f4a5abb6f8db7ce
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-05-09 10:28:54 +0200

    Dragon lifebar fix

--- Warcraft II/Warcraft II/Dragon.cpp
+++ Warcraft II/Warcraft II/Dragon.cpp
@@ -282,6 +282,15 @@ void Dragon::Draw(SDL_Texture* sprites)
 				offset = { animation->GetCurrentFrame().w / 2.8f, animation->GetCurrentFrame().h / 2.5f };
 
 			App->printer->PrintSprite({ (int)(pos.x - offset.x), (int)(pos.y - offset.y) }, sprites, animation->GetCurrentFrame(), Layers_DragonGryphon);
+
+			if (lifeBar != nullptr)
+			{
+				lifeBar->isBlit = true;
+			}
+		}
+		else
+		{
+			lifeBar->isBlit = false;
 		}
 	}
 	//if (isSelected)
