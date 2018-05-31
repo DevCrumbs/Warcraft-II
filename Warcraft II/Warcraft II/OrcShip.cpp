8bc74d2fc23f8972e50d28b656f19a45873655ab
ValdiviaDev <d.valdiviamartinez@gmail.com>
2018-05-31 18:47:51 +0200

    A fix

--- Warcraft II/Warcraft II/OrcShip.cpp
+++ Warcraft II/Warcraft II/OrcShip.cpp
@@ -42,10 +42,6 @@ OrcShip::OrcShip(fPoint pos, iPoint size, int currLife, uint maxLife, const Unit
 	this->size = this->unitInfo.size;
 	offsetSize = this->unitInfo.offsetSize;
 
-	// IA
-	spawnTile = { singleUnit->currTile.x, singleUnit->currTile.y };
-	iPoint spawnPos = App->map->MapToWorld(spawnTile.x, spawnTile.y);
-
 	isSpawnedWave = false;
 
 	speed = orcShipInfo.unitInfo.currSpeed - 25;
--- Warcraft II/Warcraft II/j1EnemyWave.cpp
+++ Warcraft II/Warcraft II/j1EnemyWave.cpp
@@ -40,7 +40,7 @@ bool j1EnemyWave::Start()
 
 	// Reset waves general info
 	totalWaves = 0;
-	isActiveWaves = false;
+	isActiveWaves = true;
 	totalPhasesOfCurrWave = 0;
 	phasesOfCurrWave = 0;
 	isStartWave = false;
